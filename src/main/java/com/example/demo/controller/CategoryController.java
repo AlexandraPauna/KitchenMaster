783bdcbbfe0dd0686e346fd5ca3a9aca5947b5b9
Mihaela Ivan <ivanmihaela.04@yahoo.com>
2020-05-20 20:43:23 +0300

    Category: Index&New&Delete

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,21 +1,63 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Category;
+import com.example.demo.model.Recipe;
+import com.example.demo.model.User;
 import com.example.demo.service.CategoryService;
 import com.example.demo.service.RecipeService;
+import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomCollectionEditor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+import java.util.*;
 
 @Controller
 public class CategoryController {
     @Autowired
     CategoryService categoryService;
 
+    @Autowired
+    UserService userService;
+
     @RequestMapping(value = "/category/index", method = RequestMethod.GET)
     public String allCategories(Model model) {
         model.addAttribute("categories", categoryService.getAllCategories());
         return "/category/index";
     }
+
+    @RequestMapping(value = "/category/new", method = RequestMethod.GET)
+    public String newCategory(Model model) {
+        Category category = new Category();
+        model.addAttribute("category", category);
+        return "/category/new";
+    }
+
+    @RequestMapping(value = "/category/new", method = RequestMethod.POST)
+    public String savedCategory(@Valid Category category, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "error";
+        }
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByUserName(auth.getName());
+
+        Category savedCategory = categoryService.saveCategory(category);
+        allCategories(model);
+        return "/category/index";
+    }
+
+    @RequestMapping("category/{id}/delete")
+    public String deleteById(@PathVariable String id){
+        categoryService.deleteById(Integer.valueOf(id));
+        return "redirect:/category/index";
+    }
+
 }
